acf294a398fe0b0add2caf8db2721bb5f695351f
Alexander Lichter <manniL@gmx.net>
2018-01-30 19:41:20 +0100

    Add live coding

--- src/assets/livecoding.js
+++ src/assets/livecoding.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 /*
 
 0. Build own map/filter with reduce
@@ -8,3 +9,51 @@
 4. Do you have any ideas or wishes?
 
 */
+
+const parameters = {query: 'i+search+things', order: 'asc', limit: '10'}
+//http://example.org/?query=...&order=...&limit=10
+
+const url = 'http://example.org/'
+
+let queryStrings = Object.keys(parameters).map(key => key + '=' + parameters[key]).join('&')
+
+//console.log(url + '?' + queryStrings)
+
+/*
+const filter = (collection, f) => {
+  return collection.reduce((acc, o) => {
+    if (f(o)) {
+      return acc.concat(o)
+    }
+    return acc
+  }, [])
+}
+
+const numbers = [1, 2, 3, 4]
+const jeven = n => !(n % 2)
+
+console.log(filter(numbers, jeven))
+*/
+
+const sum = (collection) => {
+  return collection.reduce((acc, number) => acc + number)
+}
+
+const idGreater70 = (city) => city.id > 70
+
+const processData = ({data}) => console.log(sum(data.filter(idGreater70).map(city => city.users)))
+
+//Sum of users where ID > 70
+
+axios.get('https://raw.githubusercontent.com/fsr/php-lessons/master/code/locations.json')
+  .then(processData)
+
+//remove duplicates
+
+removeDuplicatePrimitives = (collection) => {
+  return collection.reduce((acc, o) => {
+    return !acc.includes(o) ? acc.concat(o) : acc
+  }, [])
+}
+
+//console.log(removeDuplicatePrimitives([1, 2, 2, 3, 1, 5, 4, 3, 1, 1, 1, 10, 42]))
